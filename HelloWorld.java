d084c5472f5b421ee1680da1bfdf1622a3eb6509
Anastasia Lagopanagiotopoulou <t8220075@aueb.gr>
2023-10-19 12:42:36 +0300

    Ebala ki egw to onoma mou

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String [] args) {
         System.out.println("Hello World");
         System.out.println("We are Kostas and Gregoris");
+        System.out.println("I did it too! (Anastasia)");
     }
 }
